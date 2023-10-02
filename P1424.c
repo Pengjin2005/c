e1cbc7a0ef9190f579f1e4c55a1fffe4090f537c
PengJin <pengjin@smail.nju.edu.cn>
2023-10-02 10:17:35 +0800

    suspend P1424

--- P1424.c
+++ P1424.c
@@ -3,17 +3,17 @@
 int main()
 {
     unsigned long long x, n, sum = 0; //注意数据类型的取值范围
-    scanf("%d %d",&x,&n);
+    scanf("%lld %lld",&x,&n);
     int weeks;
     weeks = n/7;
     sum += 250*5*weeks;
     n -= 7*weeks;
     if (x + n >= 7)
-        sum += (n - 1)*250;
+        sum += (n-1)*250;
     else if (x + n == 6)
         sum += (n )*250;
     else
         sum += (n+1)*250;
-    printf("%d",sum);
+    printf("%lld",sum);
     return 0;
 }
--- P1424_v2.c
+++ P1424_v2.c
@@ -4,15 +4,15 @@
 
 int main()
 {
-    long long n, s;
+    long long n, s = 0;
     int x;
     scanf("%d %lld",&x,&n);
     for (int i = 0; i < n; i++)
     {
-        if (x != 6 && x != 7)
-            s += 250;
         if (x == 7)
             x = 1;
+        if (x != 6 && x != 7)
+            s += 250;
         x++;
     }
     printf("%lld",s);
