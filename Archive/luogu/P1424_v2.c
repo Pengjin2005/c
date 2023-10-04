5985703ecbebc21730ddcbecba3182b8f27a8295
PengJin <pengjin@smail.nju.edu.cn>
2023-10-04 09:28:18 +0800

    Finish P1424

--- Archive/luogu/P1424_v2.c
+++ Archive/luogu/P1424_v2.c
@@ -4,15 +4,18 @@
 
 int main()
 {
-    int x,n,sum = 0;
-    scanf("%d %d",&x,&n);
-    for (int i =0;i<n;i++)
+    long long x,n,sum;
+    sum = 0;
+    scanf("%lld %lld",&x,&n);
+    for (int i =0;i<n;++i)
     { 
         if (x!=6&&x!=7)
             sum += 250;
         if (x == 7)
             x = 1;
+        else //此处else且不能忘记，不然就会跳过所有周一！！！
+            x++;
     }
-    printf("%d",sum);
+    printf("%lld",sum);
     return 0;
 }
